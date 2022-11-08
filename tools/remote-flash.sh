f2f68e4660ca8957363139d97ba5dac81ee8718d
Stefan Reichhard <stefan@reichhard.info>
2022-11-08 17:14:05 +0100

    adjust remote-flash path and addr

--- tools/remote-flash.sh
+++ tools/remote-flash.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-ADDR=0x01
+ADDR=1
 
 # switch into bootloader
-./examples/mainmode-switch.py $ADDR
+./examples/pymodbus3.x/mainmode-switch.py $ADDR
 
 sleep .5
 
@@ -13,5 +13,5 @@ sleep .5
 sleep .5
 
 # switch back to main
-./examples/bootmode-switch.py $ADDR
+./examples/pymodbus3.x/bootmode-switch.py $ADDR
 
