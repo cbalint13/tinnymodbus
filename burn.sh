c5fd5448a5f58dbaa5ae0a254500d6ff4b3861d3
no-ahnu <156094214+no-ahnu@users.noreply.github.com>
2024-01-13 14:23:10 +0100

    Update burn.sh (#27)
    Fix missing space

--- burn.sh
+++ burn.sh
@@ -9,4 +9,4 @@
 FUSE_FLAGS='-U lfuse:w:0xE2:m -U hfuse:w:0xDD:m -U efuse:w:0xFE:m'
 
 avrdude -c usbasp -P usb -p attiny85 $FUSE_FLAGS -U flash:w:boot.hex -U eeprom:w:boot.eep -B3
-#avrdude -c stk500v2 -P /dev/ttyACM0 -p attiny85 $FUSE_FLAGS-U flash:w:boot.hex -U eeprom:w:boot.eep -B3
+#avrdude -c stk500v2 -P /dev/ttyACM0 -p attiny85 $FUSE_FLAGS -U flash:w:boot.hex -U eeprom:w:boot.eep -B3
